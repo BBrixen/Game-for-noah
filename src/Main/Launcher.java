f6f5f7c83030ace716f80cc6e2e76e709b35303d
BBrixen <bbrixen@icloud.com>
2018-08-22 15:26:50 -0700

    test

--- src/Main/Launcher.java
+++ src/Main/Launcher.java
@@ -5,7 +5,9 @@ import Main.Game;
 public class Launcher {
 
     public static void main(String[] args) {
+        //this is the main class where you start the game
         Game game = new Game("Test Game", 1500, 1000);
         game.start();
     }
 }
+
