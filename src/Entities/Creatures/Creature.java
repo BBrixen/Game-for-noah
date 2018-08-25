273201197820610aa1bbfa2d8ce8ef05a994b94f
PenguinArchmage <nqsmith02@gmail.com>
2018-08-24 20:19:26 -0700

    .

--- src/Entities/Creatures/Creature.java
+++ src/Entities/Creatures/Creature.java
@@ -14,4 +14,6 @@ public abstract class Creature extends Entity {
     }
 
 
+
+
 }
---
+++ src/Entities/Creatures/Infantry.java
@@ -0,0 +1,20 @@
+package Entities.Creatures;
+
+import java.awt.*;
+
+public class Infantry extends Creature{
+
+    public Infantry(float x, float y) {
+        super(x, y);
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics graphics) {
+
+    }
+}
